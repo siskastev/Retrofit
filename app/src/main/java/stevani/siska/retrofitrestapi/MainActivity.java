f751a34750d1986d8a5995194c7300e5e61dc05c
Siska Stevani <siska.stevani@gmail.com>
2018-11-20 12:52:13 -0800

    retrofit commit

---
+++ app/src/main/java/stevani/siska/retrofitrestapi/MainActivity.java
@@ -0,0 +1,55 @@
+package stevani.siska.retrofitrestapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import stevani.siska.retrofitrestapi.Adapters.MoviesAdapter;
+import stevani.siska.retrofitrestapi.Models.Movie;
+import stevani.siska.retrofitrestapi.Models.MovieResponse;
+import stevani.siska.retrofitrestapi.Rest.ApiClient;
+import stevani.siska.retrofitrestapi.Rest.ApiInterface;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static String API_KEY = "ab0ddbae04c19507517f678c05cba72e";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from themoviedb.org", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<MovieResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse>call, Response<MovieResponse> response) {
+                int statusCode = response.code();
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<MovieResponse>call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/stevani/siska/retrofitrestapi/Rest/ApiInterface.java
@@ -0,0 +1,16 @@
+package stevani.siska.retrofitrestapi.Rest;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import stevani.siska.retrofitrestapi.Models.MovieResponse;
+
+public interface ApiInterface {
+
+        @GET("movie/top_rated")
+        Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+        @GET("movie/{id}")
+        Call<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
